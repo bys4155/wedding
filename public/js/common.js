07a2c53f843c7d93bb66429f20b2d6afa4e62345
배영수 <bys4155@naver.com>
2023-06-09 15:35:49 +0900

    no comment

---
+++ public/js/common.js
@@ -0,0 +1,13 @@
+$('.btnCopy').on('click', function(e){
+    var regex = /[^0-9]/g;
+    var bankValue = $(this).parent().find("span").html();
+    var tempInput = document.createElement('input');
+    tempInput.value = bankValue.replace(regex, "");
+    document.body.appendChild(tempInput);
+
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    alert(bankValue + '\n복사완료');
+});
