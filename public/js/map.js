991383674f28526452dfd2e5cdede408ae4d6552
배영수 <bys4155@naver.com>
2023-06-12 18:08:09 +0900

    no comment

--- public/js/map.js
+++ public/js/map.js
@@ -3,13 +3,10 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 var kakaoNavi = {
      my1 : function () {
-         var x = "37.52831042067139";
-         var y = "126.89654331115185";
-        // alert("X :"+ x +"\n" +"Y :" + y);
          Kakao.Navi.start({
              name:"에이치피오 에이치커피",
-             x: x,
-             y: y,
+             x: 37.52831042067139,
+             y: 126.89654331115185,
              coordType:'wgs84'
          })
      }
